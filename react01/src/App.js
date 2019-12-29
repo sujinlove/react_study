3ff163c406b6f25667f24f9cc58c8ee9b6433a90
sujinlove <tnwls2274@naver.com>
2019-12-29 16:22:19 +0900

    app.js index.js

--- react01/src/App.js
+++ react01/src/App.js
@@ -1,6 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+/*
+필요한 패키지 참조하기
+- 패키지 경로에 "./" 나 "../" 가 있는경우
+  -> 현재 소스를 기준으로 한 상대경로로 소스파일을 참조함
+- 패키지 경로가 단어로 시작되는 경우
+  -> node_modules에서 패키지를 검색하여 참조함
+
+  import React from 'react';
+
+*/
+
+/**
+ * App이라는 이름의 함수형 컴포넌트(재사용 가능한 HTML 조각단위) 정의
+ *
+ * 프로젝트에서 컴포넌트를 렌더링 (화면에 출력) 하면 함수에서 반환하고 있는 내용이 브라우저에 나타난다.
+ * 반환되는 HTML코드는 JSX 문법을 사용한다.
+ *
+ * JSX --> XML과 비슷한 React 전용 Javascript 확장 문법.
+ *
+ * const App = () => <h1>Hello React</h1>
+ */
 
 function App() {
   return (
--- react01/src/index.js
+++ react01/src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/**
+ * 프로그램 시작점
+ * - 향후 Redux라는 패키지를 사용하기 전까지는 작업 안함.
+ */
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+/*
+컴포넌트를 페이지에 렌더링한다.
+
+App.js 에서 정의한 'App'이라는 이름의 컴포넌트를 HTML 태그처럼 사용한다.
+-> 첫번쨰 파라미터: 사용할 컴포넌트
+-> 두번째 파라미터: 컴포넌트를 출력할 public/index.html 페이지에 정의되어 있는 요소
+
+프로그램 실행시 http://localhost:3000 에 대응되는 위치가 public폴더
+*/
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
