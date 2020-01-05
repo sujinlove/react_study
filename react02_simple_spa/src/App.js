33670ed788a85f8235ac51ab2ffdf8fa36adae9b
sujinlove <tnwls2274@naver.com>
2020-01-05 11:13:33 +0900

    DepartmentGet.js update / parameter console check

--- react02_simple_spa/src/App.js
+++ react02_simple_spa/src/App.js
@@ -5,22 +5,27 @@ import { Route, Link } from "react-router-dom";
 
 import Home from "./components/Home"; // 각 컴포넌트가 페이지 역할을 할 것임.
 import About from "./components/About";
+import DepartmentGet from "./components/DepartmentGet";
 
 const App = () => {
   return (
     <div>
       <h1>simple page application</h1>
 
+      {/*--- <a> : 페이지 이동 / <Link> : 컴포넌트가 변경 */}
       <Link to="/">[ Home ]</Link>
       <Link to="/about">[ About ]</Link>
 
-      {}
+      {/* HTTPS GET 파라미터를 포함하는 링크 구성 ( 한글사용 X ) */}
+      <Link to="/department_get?deptno=101&msg=hello">[ DepartmentGet 1 ]</Link>
+      <Link to="/department_get?deptno=102&msg=world">[ DepartmentGet 2 ]</Link>
 
       {/* ------ 페이지로 사용될 컴포넌트들 명시하기 ------ */}
       {/* 첫 페이지로 사용되는 컴포넌트의 경우 exact={true}를 명시해야 한다. */}
 
       <Route path="/" component={Home} exact={true} />
       <Route path="/about" component={About} />
+      <Route path="/departmentget" component={DepartmentGet} />
     </div>
   );
 };
--- react02_simple_spa/src/components/DepartmentGet.js
+++ react02_simple_spa/src/components/DepartmentGet.js
@@ -2,9 +2,12 @@
     DepartmentGet.js
     - HTTP GET 파라미터를 전달받도록 구성된 페이지
 */
-
 import React from "react";
-import PropTypes from "prop-types";
+
+// QueryString 값을 식별할 수 있는 패키지 참조
+// --> 참조 에러시 "yarn add qs"
+// --> 혹은 "npm install qs --save"
+import qs from "qs";
 
 /*
     DepartmentGet
@@ -16,9 +19,29 @@ import PropTypes from "prop-types";
 */
 
 const DepartmentGet = props => {
+  console.group("DepartmentGet");
+
+  //props 값 전체 확인
+  console.log(props);
+
+  /* 컴포넌트로 전달되는 props에는 기본적으로 location 객체가 포함되어 있다. */
+  // --> QueryString 값을 받아온다.
+  const queryString = props.location.search;
+  console.log(queryString);
+
+  console.log("test");
+  /* Query String을 JSON 객체 형태로 변환  */
+  const query = qs.parse(queryString, {
+    ignoreQueryPrefix: true // 맨 앞의 "?" 제거 옵션
+  });
+  console.log(query);
+
+  console.log("요청된 학과 번호 값=%s (%s)", query.deptno, typeof query.deptno);
+  console.log("요청된 메세지 내용 값=%s (%s)", query.msg, typeof query.msg);
+
+  console.groupEnd();
+
   return <div></div>;
 };
-
-DepartmentGet.propTypes = {};
 
 export default DepartmentGet;
