04d14cc1124d495617a1e8dff5bdecd18da85889
sujinlove <tnwls2274@naver.com>
2020-01-05 12:35:07 +0900

    Creat project react03_jsx

---
+++ react03_jsx/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>03-jsx</h1>
+      {/* Link 구성 */}
+      <hr />
+
+      {/* 각 예제별 페이지 Route 적용 */}
+    </div>
+  );
+}
+
+export default App;
