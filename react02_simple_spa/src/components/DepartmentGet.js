6dc670d7086d4057ba5ea7d256eafa89ea5d40e2
sujinlove <tnwls2274@naver.com>
2020-01-05 10:40:16 +0900

    create DepartmentGet & DepartmentPath js

---
+++ react02_simple_spa/src/components/DepartmentGet.js
@@ -0,0 +1,24 @@
+/*
+    DepartmentGet.js
+    - HTTP GET 파라미터를 전달받도록 구성된 페이지
+*/
+
+import React from "react";
+import PropTypes from "prop-types";
+
+/*
+    DepartmentGet
+
+    컴포넌트에게 전달되는 부가정보들은 props라는 이름의 파라미터에 모아서 전달된다.
+    (JSON)
+
+    @param {*} props
+*/
+
+const DepartmentGet = props => {
+  return <div></div>;
+};
+
+DepartmentGet.propTypes = {};
+
+export default DepartmentGet;
---
+++ react02_simple_spa/src/components/DepartmentPath.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const DepartmentPath = props => {
+  return <div></div>;
+};
+
+DepartmentPath.propTypes = {};
+
+export default DepartmentPath;
