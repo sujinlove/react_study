a6ff5d3fe6963b51822f6d8021ab8dc44bd2c5a1
sujinlove <tnwls2274@naver.com>
2020-01-12 11:34:06 +0900

    double url link

--- react02_simple_spa/src/App.js
+++ react02_simple_spa/src/App.js
@@ -16,7 +16,8 @@ const App = () => {
 
       {/*--- <a> : 페이지 이동 / <Link> : 컴포넌트가 변경 */}
       <Link to="/">[ Home ]</Link>
-      <Link to="/about">[ About ]</Link>
+      <Link to="/about">[ About (1) ]</Link>
+      <Link to="/introduce">[ About (2) ]</Link>
 
       {/* HTTPS GET 파라미터를 포함하는 링크 구성 ( 한글사용 X ) */}
       <Link to="/department_get?deptno=101&msg=hello">[ DepartmentGet 1 ]</Link>
@@ -30,7 +31,9 @@ const App = () => {
       {/* 첫 페이지로 사용되는 컴포넌트의 경우 exact={true}를 명시해야 한다. */}
       <Switch>
         <Route path="/" component={Home} exact={true} />
-        <Route path="/about" component={About} />
+
+        {/* 한페이지에 두 개 이상의 URL을 적용할 경우 배열 형시으로 처리함 */}
+        <Route path={["/about", "/introduce"]} component={About} />
         <Route path="/department_get" component={DepartmentGet} />
 
         {/* Path 파라미터는 URL 형식에 변수의 위치와 이름을 정해줘야 한다. */}
