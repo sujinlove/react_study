96443cc0b2670cf4452877691709c14776ab742f
sujinlove <tnwls2274@naver.com>
2020-01-12 12:36:08 +0900

    add new component file

--- react05_component/src/App.js
+++ react05_component/src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { Route, Link, Switch } from "react-router-dom";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <div className="App">
+      <h1>05-component</h1>
+    </div>
+  );
+};
 
 export default App;
