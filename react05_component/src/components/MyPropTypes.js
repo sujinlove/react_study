5e76dbf8b38daf75c60d54963c43fc91185ae38b
sujinlove <tnwls2274@naver.com>
2020-01-12 14:28:49 +0900

    MyPropTypes

--- react05_component/src/components/MyPropTypes.js
+++ react05_component/src/components/MyPropTypes.js
@@ -1,9 +1,15 @@
 import React from "react";
+import MyPropTypesSub from "./MyPropsTypesSub";
 
 const MyPropTypes = () => {
   return (
     <div>
       <h2>여기는 MyPropTypes </h2>
+
+      {/* 컴포넌트에게 props 전달하기 */}
+      <MyPropTypesSub name="민호" age="19" hobby="사진찍기" />
+      <MyPropTypesSub name="수영" age="스물한살" hobby="사진찍기" />
+      <MyPropTypesSub name="철민" age="22" />
     </div>
   );
 };
