eff2cab5083e5183ece67ddc70561df9ca666a68
sujinlove <tnwls2274@naver.com>
2020-02-02 15:22:07 +0900

    create react07 Simple Ajax project

---
+++ react07_simple_ajax/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <h1>Simple Ajax</h1>
+    </div>
+  );
+}
+
+export default App;
