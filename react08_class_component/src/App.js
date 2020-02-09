251dcf1bef7a447334fc836f862ecdfefe8b3b52
sujinlove <tnwls2274@naver.com>
2020-02-09 10:16:30 +0900

    Create class compontent Project

---
+++ react08_class_component/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>react08 class components</h1>
+    </div>
+  );
+};
+
+export default App;
