590d14b9163630099a2cdb8dc9a041d1b5d93590
sujinlove <tnwls2274@naver.com>
2020-02-09 16:18:19 +0900

    route

--- react09_class_ajax/src/App.js
+++ react09_class_ajax/src/App.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, NavLink, Switch } from "react-router-dom";
 
 function App() {
   return (
