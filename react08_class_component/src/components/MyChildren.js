9abcfb44942c4dab43bef05d84eccc6fcbb47190
sujinlove <tnwls2274@naver.com>
2020-02-09 11:35:59 +0900

    change myChildren & myChildrenSub to class class component

--- react08_class_component/src/components/MyChildren.js
+++ react08_class_component/src/components/MyChildren.js
@@ -1,12 +1,23 @@
 import React from "react";
 
 import MychildrenSub from "./MyChildrenSub";
+import MyChildrenSub from "./MyChildrenSub";
 
 class MyChildren extends React.Component {
   render() {
     return (
       <div>
         <h2>MyChildren</h2>
+
+        <MyChildrenSub width={400} height={100}>
+          Hello World
+        </MyChildrenSub>
+
+        <MyChildrenSub width={300} height={80}>
+          Hello React
+        </MyChildrenSub>
+
+        <MyChildrenSub width={200} height={50} />
       </div>
     );
   }
--- react08_class_component/src/components/MyChildrenSub.js
+++ react08_class_component/src/components/MyChildrenSub.js
@@ -7,21 +7,42 @@ import PropTypes from "prop-types";
 //const MyChildrenSub = props => {
 //    const { width, height, children } = props;
 
-const MyChildrenSub = ({ width, height, children }) => {
-  const myStyle = {
-    width: width + "px",
-    height: height + "px",
-    border: "5px solid #d5d5d5",
-    wmargin: "10px"
+class MyChildrenSub extends React.Component {
+  static propTypes = {
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired,
+    children: PropTypes.string
   };
 
-  return (
-    <div>
-      <h3>MyChildrenSub</h3>
-      <p style={myStyle}>{children}</p>
-    </div>
-  );
-};
+  static defaultProps = {
+    width: 300,
+    height: 100,
+    children: "내용이 없습니다."
+  };
+
+  // inline css 를 정의하기 위한 getter 함수
+  get myStyle() {
+    // props에 포함되어 있는 넓이, 높이값을 비구조 문법으로 추출
+
+    const { width, height } = this.props;
+
+    return {
+      width: width + "px",
+      height: height + "px",
+      border: "5px solid #d5d5d5",
+      wmargin: "10px"
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>MyChildrenSub</h3>
+        <p style={this.myStyle}>{this.props.children}</p>
+      </div>
+    );
+  }
+}
 
 // 속성들에 대한 타입 정의
 MyChildrenSub.propTypes = {
