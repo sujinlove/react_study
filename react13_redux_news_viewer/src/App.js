cded30da180003680bd9b50b0175e5fa5a33ebd9
sujinlove <tnwls2274@naver.com>
2020-02-23 16:14:36 +0900

    news viewer comments

--- react13_redux_news_viewer/src/App.js
+++ react13_redux_news_viewer/src/App.js
@@ -8,6 +8,7 @@ const App = () => {
       <h1>Redux News Viewer</h1>
       <Switch>
         {/** Path 파라미터로 카테고리 값을 받는 페이지 구성 */}
+        {/** ":/변수이름?" 에서 물음표는 해당 변수가 선택적이라는 의미 */}
         <Route path="/:category?" component={NewsPage} />
       </Switch>
     </div>
