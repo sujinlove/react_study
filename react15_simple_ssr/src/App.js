d2594b32b5bc662c3137be45bd62be29bed481d7
sujinlove <tnwls2274@naver.com>
2020-03-08 10:47:48 +0900

    simple ssr / 웹팩 설정 전 초기화 처리

---
+++ react15_simple_ssr/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Simple SSR</h1>
+    </div>
+  );
+};
+
+export default App;
